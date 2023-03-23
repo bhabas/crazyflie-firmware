f269a58b69e2a65ff66ccdd488039d2a7ab77b51
bhabas <bhabas@psu.edu>
2023-03-23 16:52:08 -0400

    Updated example task to receive values over UART1 from AI-Deck

--- src/modules/src/example_task.c
+++ src/modules/src/example_task.c
@@ -4,9 +4,18 @@
 #include "queue.h"
 #include "static_mem.h"
 #include "task.h"
+#include "uart1.h"
+
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #define DEBUG_MODULE "CUSTOM_TASK"
+#define ARRAY_LENGTH 3
 
+
+uint8_t tempArray[sizeof(float) * ARRAY_LENGTH]; // temperary array of uint8_t to store the received data
+float myArray[ARRAY_LENGTH] = {0.0f,0.0f,0.0f}; // array to store the 3 float values
 
 static xQueueHandle inputQueue;
 STATIC_MEM_QUEUE_ALLOC(inputQueue, 1, sizeof(int));
@@ -18,6 +27,7 @@ static bool isInit = false;
 
 void exampleTaskInit() {
   inputQueue = STATIC_MEM_QUEUE_CREATE(inputQueue);
+  uart1Init(UART1_BAUDRATE);
   // TODO
   STATIC_MEM_TASK_CREATE(exampleTask, exampleTask, EXAMPLE_TASK_NAME, NULL, EXAMPLE_TASK_PRI);
   isInit = true;
@@ -29,12 +39,15 @@ bool exampleTaskTest() {
 }
 
 static void exampleTask(void* parameters){
-    DEBUG_PRINT("Example task main function is running!\n");
+    DEBUG_PRINT("--- Example task main function is running! ---\n");
     while (true) {
-        int input;
-        if (pdTRUE == xQueueReceive(inputQueue, &input, portMAX_DELAY)) {
-            DEBUG_PRINT("Value: %d\n",input);
-        }
+
+      uart1GetBytesWithDefaultTimeout(sizeof(tempArray),tempArray);
+
+      for (int i = 0; i < ARRAY_LENGTH; i++) {
+      memcpy(&myArray[i], &tempArray[i * sizeof(float)], sizeof(float));
+      }
+      DEBUG_PRINT("Value1: %f \t Value2: %f \t Value3: %f\n",(double)myArray[0],(double)myArray[1],(double)myArray[2]);
     }
 }
 
