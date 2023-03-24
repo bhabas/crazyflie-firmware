04a315bccd72a4c2882346b434f49171581151a2
bhabas <bhabas@psu.edu>
2023-03-24 16:26:19 -0400

    Updating header files for AIDeck comm task

--- src/modules/interface/aideck_comm_task.h
+++ src/modules/interface/aideck_comm_task.h
@@ -1,8 +1,21 @@
 #pragma once
 
+#include <string.h>
+#include <stdint.h>
 #include <stdbool.h>
 
-extern float value_3;
+#include "config.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "static_mem.h"
+#include "task.h"
+
+#include "uart1.h"
+
+
+extern float Theta_x_cam_est;
+extern float Theta_y_cam_est;
+extern float Theta_z_cam_est;
 
 /**
  * Initialization of AI-Deck Communication task
--- src/modules/src/aideck_comm_task.c
+++ src/modules/src/aideck_comm_task.c
@@ -1,25 +1,17 @@
-#include "config.h"
-#include "debug.h"
-#include "FreeRTOS.h"
-#include "queue.h"
-#include "static_mem.h"
-#include "task.h"
-#include "uart1.h"
 #include "aideck_comm_task.h"
+#include "debug.h"
 
-#include <string.h>
-#include <stdint.h>
-#include <stdbool.h>
+#define DEBUG_MODULE "AIDECK_COMM_TASK"
 
-#define DEBUG_MODULE "CUSTOM_TASK"
-
-// DEFINE AND INITIALIZE ARRAYS
-#define ARRAY_LENGTH 3
+// DEFINE AND INITIALIZE ARRAY
 static bool isInit = false;
+#define ARRAY_LENGTH 3
 static uint8_t data_Array[sizeof(float) * ARRAY_LENGTH]; // Byte-String data array
 float value_Array[ARRAY_LENGTH]; // Value array to store the received float values
 
-float value_3 = 0.0;
+float Theta_x_cam_est = 0.0f;
+float Theta_y_cam_est = 0.0f;
+float Theta_z_cam_est = 0.0f;
 
 static xQueueHandle taskQueue; // Not entirely sure but is part of task code
 STATIC_MEM_QUEUE_ALLOC(taskQueue, 1, sizeof(int));
@@ -29,38 +21,42 @@ STATIC_MEM_TASK_ALLOC(AIDeckComm_Task, AIDECK_COMM_STACKSIZE);
 
 void AIDeckComm_TaskInit() {
 
-  uart1Init(UART1_BAUDRATE); // Initialize UART1 connection
+    // INITIALIZE UART1 CONNECTION
+    uart1Init(UART1_BAUDRATE); 
 
-  // INITIALIZE TASK
-  taskQueue = STATIC_MEM_QUEUE_CREATE(taskQueue);
-  STATIC_MEM_TASK_CREATE(AIDeckComm_Task, AIDeckComm_Task, AIDECK_COMM_TASK_NAME, NULL, AIDECK_COMM_TASK_PRI);
+    // INITIALIZE TASK
+    taskQueue = STATIC_MEM_QUEUE_CREATE(taskQueue);
+    STATIC_MEM_TASK_CREATE(AIDeckComm_Task, AIDeckComm_Task, AIDECK_COMM_TASK_NAME, NULL, AIDECK_COMM_TASK_PRI);
 
-  isInit = true;
+    isInit = true;
 }
 
 
 bool AIDeckComm_TaskTest() {
-  return isInit;
+
+    return true;
 }
 
 
 void AIDeckComm_Task(void* parameters){
   
-    DEBUG_PRINT("--- AI-DECK COMM task main function is running! ---\n");
+    DEBUG_PRINT("--- AIDeckComm_Task is running! ---\n");
 
     while (true) {
 
-      // RECIEVE BYTE-STRING OVER UART1
-      uart1GetBytesWithDefaultTimeout(sizeof(data_Array),data_Array);
+        // RECIEVE BYTE-STRING OVER UART1
+        uart1GetBytesWithDefaultTimeout(sizeof(data_Array),data_Array);
 
-      // CONVERT BYTE-STRING TO FLOAT ARRAY
-      for (int i = 0; i < ARRAY_LENGTH; i++) {
+        // CONVERT BYTE-STRING TO FLOAT ARRAY
+        for (int i = 0; i < ARRAY_LENGTH; i++) {
         memcpy(&value_Array[i], &data_Array[i * sizeof(float)], sizeof(float));
-      }
+        }
 
-      value_3 = value_Array[0];
+        Theta_x_cam_est = value_Array[0];
+        Theta_y_cam_est = value_Array[1];
+        Theta_z_cam_est = value_Array[2];
 
-      // PRINT FLOAT ARRAY TO CONSOLE [DEBUG]
-      DEBUG_PRINT("Value1: %f \t Value2: %f \t Value3: %f\n",(double)value_Array[0],(double)value_Array[1],(double)value_Array[2]);
+        // PRINT FLOAT ARRAY TO CONSOLE [DEBUG]
+        // DEBUG_PRINT("Value1: %f \t Value2: %f \t Value3: %f\n",(double)value_Array[0],(double)value_Array[1],(double)value_Array[2]);
     }
 }
